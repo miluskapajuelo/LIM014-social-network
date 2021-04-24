f020a036b43280175a1a5284b3f101981cd4c81f
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-04-24 09:30:27 -0500

    se agrega html a la vista home

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,22 @@
 export default () => {
-  const viewRegister = '<h1>CHICAS CEREZAS</h1>';
+  const viewRegister = `<header id="main-header" class ="header">
+  <div class="menu-bar">
+    <a href="#" class="bt-menu">W-coding</a>
+  </div>
+  <nav class="list">
+    <ul >
+      <li id="Profile">
+        <a href="#"><span class="material-icons">account_circle</span>Profile</a>
+      </li>
+      <li id="logOut"><a href="#"><span class="material-icons">logout</span>Log Out</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="menu">
+    <span class="material-icons">menu</span>
+  </div>
+</header>
+<!-- main header -->`;
   const reg = document.getElementById('main-login');
   reg.innerHTML = '';
   reg.innerHTML = viewRegister;
