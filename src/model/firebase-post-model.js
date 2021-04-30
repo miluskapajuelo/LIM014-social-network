fe18664a0d78b72e733ce8d2e60b3ff410253a19
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-04-30 17:02:29 -0500

    se agrega modal a la edición de la publicación

--- src/model/firebase-post-model.js
+++ src/model/firebase-post-model.js
@@ -1,23 +1,24 @@
 const fs = firebase.firestore();
 
-const getNameUser = () => new Promise((resolve) => {
-  if (firebase.auth().currentUser.displayName === null) {
-    const prueba = fs.collection('users').get();
-    prueba.then((omg) => {
-      omg.forEach((data) => {
-        if (data.data().id === firebase.auth().currentUser.uid) {
-          resolve(data.data().user);
-        }
+const getNameUser = () =>
+  new Promise((resolve) => {
+    if (firebase.auth().currentUser.displayName === null) {
+      const prueba = fs.collection("users").get();
+      prueba.then((omg) => {
+        omg.forEach((data) => {
+          if (data.data().id === firebase.auth().currentUser.uid) {
+            resolve(data.data().user);
+          }
+        });
       });
-    });
-  } else {
-    resolve(firebase.auth().currentUser.displayName);
-  }
-});
+    } else {
+      resolve(firebase.auth().currentUser.displayName);
+    }
+  });
 export default function addPost(post) {
-  const dateP = firebase.firestore.FieldValue.serverTimestamp()
+  const dateP = firebase.firestore.FieldValue.serverTimestamp();
   getNameUser().then((msg) => {
-    fs.collection('post').add({
+    fs.collection("post").add({
       publication: post,
       email: firebase.auth().currentUser.email,
       uid: firebase.auth().currentUser.uid,
@@ -33,11 +34,11 @@ export function removePost(id) {
     elemento.addEventListener("click", (e) => {
       e.preventDefault();
       // Modal, estás segura que quieres eliminar el post?
-      fs.collection('post')
+      fs.collection("post")
         .doc(id)
         .delete()
         .then(() => {
-          alert('Document successfully deleted!');
+          alert("Document successfully deleted!");
         })
         .catch((error) => {
           console.error("Error removing document: ", error);
@@ -45,19 +46,20 @@ export function removePost(id) {
     });
   });
 }
-function showModal() {
+function showModal(msg) {
   const modalMode = document.getElementById("modal-mode");
   const modalWindow = document.getElementById("modal-window");
   modalMode.classList.toggle("hide");
   modalWindow.classList.toggle("hide");
-  modalWindow.innerHTML = `   <h2>Editar publicación</h2><section id="body-modal" class="modal flex-wrap">
-      <article id="NameModal" class="font f-medium f-green one-fraction"> 
-      <p class="nameP">nombre de usuario</p>
-      <div>
-      publicación completa lista para cambiar
-      </div>
+  modalWindow.innerHTML = `   <section id="body-modal" class="contentGeneral">
+  <div class="contentArea">
+  <div><h2>Editar publicación</h2></div>
+      <div class="nameP" id="modal-username">${msg}</div>
+      <textarea id="input-edit-note"></textarea>
+      
+      <div><button id="btn-edit-note">Guardar</button></div>
       <a href="#" title="Close" class="modal-close">Close</a>
-      </div><button>Guardar</button>
+      </div>
       </section>
       `;
 
@@ -73,30 +75,37 @@ function showModal() {
 
 export function updatePost(id, post) {
   const update = document.querySelectorAll(`.editBtn-${id}`);
+  const modalMode = document.getElementById("modal-mode");
+  const modalWindow = document.getElementById("modal-window");
   update.forEach((elemento) => {
-    elemento.addEventListener("click", () => {
-      /* showModal(); */
+    elemento.addEventListener("click", (e) => {
+      e.preventDefault()
+      
+      getNameUser().then((msg)=>{showModal(msg)
+        const button = document.getElementById("btn-edit-note");
+        document.getElementById("input-edit-note").value = post;
+        button.addEventListener("click", () => {
+          var washingtonRef = fs.collection("post").doc(id);
+          const post = document.getElementById("input-edit-note").value;
+          return washingtonRef
+            .update({
+              publication: post,
+            })
+            .then(() => {
+              console.log("Document successfully updated!");
+              /* button.innerHTML = "Guardar"; */
+              modalMode.classList.toggle("hide");
+              modalWindow.classList.toggle("hide");
+              /*  document.getElementById("input-new-note").value = ""; */
+            })
+            .catch((error) => {
+              console.error("Error removing document: ", error);
+            });
+        });})
+      
 
-      const button = document.getElementById("btn-add-note");
-      document.getElementById("input-new-note").value = post;
-
-      button.innerHTML = "Editar";
-      button.addEventListener("click", () => {
-        var washingtonRef = fs.collection("post").doc(id);
-        const post = document.getElementById("input-new-note").value;
-        return washingtonRef
-          .set({
-            publication: post,
-          })
-          .then(() => {
-            console.log("Document successfully updated!");
-            button.innerHTML = "Share";
-           /*  document.getElementById("input-new-note").value = ""; */
-          })
-          .catch((error) => {
-            console.error("Error removing document: ", error);
-          });
-      });
+      /* button.innerHTML = "Habilitarse"; */
+      
     });
   });
 }
