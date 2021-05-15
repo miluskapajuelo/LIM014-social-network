35a08b52c1207a6d2d1661601c6e76cb7f47bad6
Eunice Fiorella <euniceancajima@gmail.com>
2021-05-15 17:57:26 -0500

    Haciendo mock de prueba en la parte de configuracion firebase

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,33 @@
 // importamos la funcion que vamos a testear
 // import { myFunction } from '../src/lib/index';
+import MockFirebase from 'mock-cloud-firestore';
 import { addPost } from '../src/controller/post.js';
+// import mockConfig from '../__mocks__/mocksFire.js';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof addPost).toBe('function');
+const fixtureData = {
+  __collection__: {
+    post: {
+      __doc__: {
+        12345: {
+          publication: 'Tengo miedo',
+          email: 'euncieancajima@gmail.com',
+          uid: 12345,
+          datePost: '13/05/2021',
+          user: 'Eunice',
+          likePost: [],
+          countLikes: 0,
+        },
+      },
+    },
+  },
+};
+global.firebase = MockFirebase(fixtureData);
+jest.mock('../src/configFirebase.js');
+
+describe('my mock config', () => {
+  it('debería mockear mi config', () => {
+    return addPost().then((omg) => {
+      expect(omg).toBe('Hola mundo');
+    });
   });
 });
