e7ca01bc3877eb90468fe3b8be0056583a62c1cd
Katherine-fe <kattyserranobasan@gmail.com>
2021-05-17 12:16:20 -0500

    agregando test comment

--- test/comment.spec.js
+++ test/comment.spec.js
@@ -1,6 +1,8 @@
 import MockFirebase from 'mock-cloud-firestore';
 
-import { addCommentBd, getComment /* removeCommentBd */ } from '../src/controller/comment.js';
+import {
+  addCommentBd, getComment, removeCommentBd, updateCommentBd,
+} from '../src/controller/comment.js';
 
 const fixtureData = {
   __collection__: {
@@ -32,25 +34,35 @@ global.firebase = new MockFirebase(fixtureData, {
 describe('create comments', () => {
   it('Deberia de poder agregar comentarios según post', (done) => addCommentBd('post001', 'Que lindo gatito', 'ID001', '', 'userName 1').then(() => {
     const callback = (publication) => {
-      console.log(publication);
       const result = publication.find(
-        (element) => element.publication === ('Que lindo gatito'),
+        // eslint-disable-next-line dot-notation
+        (element) => element['_data'].publication === ('Que lindo gatito'),
       );
-      expect(result.publication).toBe('Que lindo gatito');
+        // eslint-disable-next-line dot-notation
+      expect(result['_data'].publication).toBe('Que lindo gatito');
       done();
     };
-    getComment(callback, 'post001');
+    getComment('post001', callback);
   }));
 });
 
-/* describe('delete comments', () => {
-  it('Deberia de poder eliminar un comments', (done) => removeCommentBd('comment001').then(() => {
-    const callback = (publication) => {
-      const result = publication.find((element) => element.id === 'comment001');
-      expect(result).toBe(undefined);
-      done();
-    };
-    getComment(callback);
-  }));
+describe('delete comment', () => {
+  it('Deberia de poder eliminar un comentario', (done) => removeCommentBd('comment001').then(() => getComment('post001', (arrayComment) => {
+    const result = arrayComment.find(
+      // eslint-disable-next-line dot-notation
+      (element) => element['_id'] === 'comment001',
+    );
+    expect(result).toBe(undefined);
+    done();
+  })));
 });
- */
+
+describe('update comments', () => {
+  it('Deberia de poder actualizar comentarios', (done) => updateCommentBd('comment002', 'eres como Piolin').then(() => getComment('post002', (publication) => {
+    // eslint-disable-next-line dot-notation
+    const result = publication.find((element) => element['_id'] === 'comment002');
+    // eslint-disable-next-line dot-notation
+    expect(result['_data'].publication).toBe('eres como Piolin');
+    done();
+  })));
+});
